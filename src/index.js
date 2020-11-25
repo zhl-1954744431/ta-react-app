fb0f94554c1ee070db25dcf9b850bdc5924370f4
xinan <781755941@qq.com>
2020-11-25 19:14:06 +0800

    移动端适配

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import 'lib-flexible/flexible'
+import './index.css'
+
 
 ReactDOM.render(
   <React.StrictMode>
---
+++ src/view/xuenan/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../../css/one.css'
+function Header() {
+    return (
+        <header id="header">
+            <nav className="menu">
+                <a href="">导航</a>
+            </nav>
+            <h1 className="logo">miaov.com</h1>
+            <a className="user" href=""></a>
+        </header>
+    );
+}
+
+
+export default Header
